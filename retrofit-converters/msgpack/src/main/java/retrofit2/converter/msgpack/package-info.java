2c36b6114b87affbd4154eb66240f312b80190c1
Chintan Mishra <chintan.greenzone@gmail.com>
2018-05-03 17:23:43 +0530

    Eclipse auto-generated Maven project files

---
+++ retrofit-converters/msgpack/src/main/java/retrofit2/converter/msgpack/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Originally authored by CMishra at work
+ * Using their Local Machine
+ * */
+
+@ParametersAreNonnullByDefault
+package retrofit2.converter.msgpack;
+
+import javax.annotation.ParametersAreNonnullByDefault;
